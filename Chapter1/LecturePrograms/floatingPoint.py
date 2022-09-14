294520e0dc02b5f90a7649e5c0802c768260e67d
wit4401 <wtownsend4401@gmail.com>
2022-09-14 11:06:27 -0400

    rename floatingPoint.py to machineEpsilon.py

--- Chapter1/LecturePrograms/floatingPoint.py
+++
@@ -1,8 +0,0 @@
-"""
-Class Notes 09/13/2022
-
-Note: These are lecture notes and not an original property of
-my work
-
-Will Townsend
-"""
---
+++ Chapter1/LecturePrograms/machineEpsilon.py
@@ -0,0 +1,13 @@
+"""
+Problem 1.3.10
+"Using the computer and language of your choice write a 
+ program to estimate the machine epsilon (of your machine)"
+
+ Machine epsilon is a number such that any number epsilon where:
+ epsilon + 1.0 = 1.0
+"""
+
+import math
+
+
+
