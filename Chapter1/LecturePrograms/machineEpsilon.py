a21228eb62c19c3de50ab1c1ceff4ec555034983
wit4401 <wtownsend4401@gmail.com>
2022-09-14 11:56:33 -0400

    added HW problem and updated machine epsilon

--- Chapter1/LecturePrograms/machineEpsilon.py
+++ Chapter1/LecturePrograms/machineEpsilon.py
@@ -1,5 +1,6 @@
 """
 Problem 1.3.10
+
 "Using the computer and language of your choice write a 
  program to estimate the machine epsilon (of your machine)"
 
@@ -9,5 +10,26 @@ Problem 1.3.10
 
 import math
 
+# Strategy?
+# Let's iterate through 2-adic fractions as they get smaller,
+# and use a while loop to do so.
 
+# Variable Setup
+e = 1.0 # Epsilon  starting with it equaling a non-trivial 2-adic fractional number e = 1/(2^n)
+n = 1.0 # track where in the iteration we are
+
+# we need to keep the loop going until e + 1 = 1
+while(not (1==1+e)):
+    # in order to not loop forever these numbers need to get smaller
+    e = (2**(-n))
+    # print the result of the iteration
+    print('n = {},\te = {}'.format(n,e))
+    # and then increase our counter
+    n=n+1
+
+# finally print out the resulting answer
+print('\nSmallest value epsilon: {:24.24f}'.format(e))
+
+# proof that this is our true epsilon value
+print('e + 1 = {:24.24f}'.format(e+1))
 
---
+++ Chapter1/problemChp1_Sec3_Problem7.py
@@ -0,0 +1,12 @@
+"""
+Problem 1.3.7
+
+a) Using the computer of your choice, find three values 
+   a, b, and c s.t.:
+
+            (a + b) + c != a + (b + c)
+
+b) Now do this in single precision using MATLAB (or whatever 
+   computing environment is available).
+"""
+
