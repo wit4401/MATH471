0a4edddd82bbd3a8caf5fedd8b50703839da2004
wit4401 <wtownsend4401@gmail.com>
2022-09-20 15:53:32 -0400

    Added comment headers, imported libraries

--- Chapter2/LectureNotes/hornersRule.py
+++ Chapter2/LectureNotes/hornersRule.py
@@ -1,1 +1,6 @@
-print('Hello World')
+"""
+This program is an implentation of Horner's rule (Nested Multiplication) from Chapter 2.1
+
+Similiar to the psuedocode presented in the chapter just implemented in Python3
+"""
+
--- Chapter2/problem2_1_1.py
+++ Chapter2/problem2_1_1.py
@@ -1,1 +1,10 @@
-print('Hello World')
+"""
+Will Townsend
+
+Problem 2.1.???
+
+****Enter Problem Description Here***
+"""
+import math
+import numpy
+
