40aced49b73b01eb3335599a9ba61da08c179240
wit4401 <wtownsend4401@gmail.com>
2022-10-04 11:50:35 -0400

    added to eulersMehtod notes

--- Chapter2/Chp2.3/eulersMethodNotes.py
+++ Chapter2/Chp2.3/eulersMethodNotes.py
@@ -1,12 +1,30 @@
-# lectureNotes.py (10.03.2022)
+# eulersMethodNotes.py (10.03.2022)
 #
-# Euler's Method Solver (Chp 2.3)
+# Euler's Method for IVP (Chp 2.3)
 
 import math
 import numpy as np
 import matplotlib.pyplot as plt
 
-#actual functional solution to ODE:
+# Naive Eulers Method:
+#
+# f - ode function
+# t - domain (gridded) for the function
+# y0 - initial y value
+# a - start of interval of interest
+# b - end of interval of interest
+# hinv - number of subdivisions
+
+def eulersMethod(f,t,y0,a,b,hinv):
+    # Initialize numpy array
+    ret = np.arange(a,b+1/hinv,1/hinv)
+    print(ret)
+
+# Our class ODE example function
+def ode(t,y):
+    return y*(1-2*t)
+
+# Actual reference functional solution to ODE:
 def exactSol(t):
     return math.exp(t-t**2.0)
 
@@ -15,11 +33,24 @@ y0=exactSol(x0)
 
 print('x0 = {}, y0 = {}'.format(x0,y0))
 
-#choose an interval which includes our desited value y(3):
+# Choose an interval which includes our desited value y(3):
 a = x0
 b = 3.0
 
 # Determine the grid intervals:
-h = 64   #divisor for grid
-gridh = (b-a)/8
+hinv = 8   #divisor for grid
+delta = (b-a)/hinv
+
+# Print grid values
+print('delta = {}'.format(delta))
+
+# Set up plotting:
+# Create domain for plot:
+t = np.linspace(a,b,hinv,endpoint=True)
+exact = np.array([exactSol(x) for x in t])
+oSol = eulersMethod(ode, t, y0, a, b, hinv)
+
+plt.plot(t,exact,label='exact')
+plt.legend()
+plt.show()
 
