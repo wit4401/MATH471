0a40717675a158d53ba8e09ded4ad65e319171cf
wit4401 <wtownsend4401@gmail.com>
2022-10-05 11:45:00 -0400

    finished notes for eulersMethodNotes.py

--- Chapter2/Chp2.3/eulersMethodNotes.py
+++ Chapter2/Chp2.3/eulersMethodNotes.py
@@ -11,14 +11,24 @@ import matplotlib.pyplot as plt
 # f - ode function
 # t - domain (gridded) for the function
 # y0 - initial y value
-# a - start of interval of interest
-# b - end of interval of interest
-# hinv - number of subdivisions
+# delta - length of subdivisions of interval
 
-def eulersMethod(f,t,y0,a,b,hinv):
-    # Initialize numpy array
-    ret = np.arange(a,b+1/hinv,1/hinv)
-    print(ret)
+def eulersMethod(f,t,y0,delta,verbose=False):
+    # Initialize numpy array to hold the y values for our solution function
+    ret = np.arange(t[0],t[-1]+delta,delta)
+    
+    # error checking
+    if verbose:
+        print(ret)
+    # set our initial value
+    ret[0]=y0
+
+    for i in range(1,len(t),1):
+        # Eulers Method
+        ret[i]=ret[i-1]+delta*f(t[i-1],ret[i-1])
+    
+    #reuturn the values obtained through euler's method
+    return ret
 
 # Our class ODE example function
 def ode(t,y):
@@ -33,24 +43,28 @@ y0=exactSol(x0)
 
 print('x0 = {}, y0 = {}'.format(x0,y0))
 
-# Choose an interval which includes our desited value y(3):
+# Choose an interval which includes our desired value y(3):
 a = x0
 b = 3.0
 
 # Determine the grid intervals:
-hinv = 8   #divisor for grid
+hinv = 16   #divisor for grid
 delta = (b-a)/hinv
 
 # Print grid values
 print('delta = {}'.format(delta))
 
 # Set up plotting:
-# Create domain for plot:
-t = np.linspace(a,b,hinv,endpoint=True)
-exact = np.array([exactSol(x) for x in t])
-oSol = eulersMethod(ode, t, y0, a, b, hinv)
+# Create domain for plot (Notice  we should add +1  to hinv - why?):
+t = np.linspace(a,b,hinv+1,endpoint=True)
 
+oSol = eulersMethod(ode, t, y0, delta)
+exact = np.array([exactSol(x) for x in t])
+
+plt.figure(figsize=(8,6))
 plt.plot(t,exact,label='exact')
+plt.plot(t,oSol,label='oSol')
 plt.legend()
 plt.show()
+
 
