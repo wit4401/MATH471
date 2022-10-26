76b6a47a895e684c980b688cd5d91bf603d6e539
wit4401 <wtownsend4401@gmail.com>
2022-10-25 20:08:17 -0400

    added py files for coding problems for 2.6

---
+++ Chapter2/Chp2.6/problem2_6_10.py
@@ -0,0 +1,23 @@
+"""
+Problem 2.6.10
+Solve the following two system of equations (Tridiagonal Matrix):
+A)
+x1+x2=
+x1+x2+x3=
+x1+x2+x3+x4=
+x2+x3+x4=
+x3+x4=
+
+B)
+x1+x2=
+x1+x2+x3=
+x1+x2+x3+x4=
+x2+x3+x4=
+x3+x4=
+"""
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+sys.path.append('/Users/williamtownsend/Desktop/SchoolWork/Fall2022/MATH471')
+import coursefuncts.chp2 as ex
---
+++ Chapter2/Chp2.6/problem2_6_2.py
@@ -0,0 +1,15 @@
+"""
+Problem 2.6.2
+Solve the following system of equations (Tridiagonal Matrix):
+x1+x2=
+x1+x2+x3=
+x1+x2+x3+x4=
+x2+x3+x4=
+x3+x4=
+"""
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+sys.path.append('/Users/williamtownsend/Desktop/SchoolWork/Fall2022/MATH471')
+import coursefuncts.chp2 as ex
