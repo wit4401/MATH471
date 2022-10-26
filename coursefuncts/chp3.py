2e2539b414a3866497c6e257cd2f0513e0efa82e
wit4401 <wtownsend4401@gmail.com>
2022-10-26 16:22:48 -0400

    added comments for each section coursefuncts.chp3

---
+++ coursefuncts/chp3.py
@@ -0,0 +1,25 @@
+import math
+import numpy as np
+import matplotlib.pyplot as pyplot
+
+""" Chapter 3.1 The Bisection Method """
+
+""" Chapter 3.2 Newton's Method: Derivation and Examples """
+
+""" Chapter 3.3 How to Stop Newton's Method """
+
+""" Chapter 3.4 Application: Division Using Newton's Method """
+
+""" Chapter 3.5 The Newton Error Formula """
+
+""" Chapter 3.6 Newton's Method: Theory and Convergence """
+
+""" Chapter 3.7 Application: Computation of the Square Root """
+
+""" Chapter 3.8 The Secant Method: Derivation and Examples """
+
+""" Chapter 3.9 Fixed-Point Iteration """
+
+""" Chapter 3.10  Roots of Polynomials Part I """
+
+""" Chapter 3.11 Special Topics in Root Finding Methods """
