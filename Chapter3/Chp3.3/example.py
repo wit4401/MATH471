6749460e313655807ad44eeeeec86e992d05d492
wit4401 <wtownsend4401@gmail.com>
2022-11-07 18:00:18 -0500

    added to example file

--- Chapter3/Chp3.3/example.py
+++ Chapter3/Chp3.3/example.py
@@ -1,4 +1,5 @@
 # Example file to test the another_newtons_method created using the knowledge from section 3.3
+# compares the two functions by applying f(x)=e^x-x^2 x0=0.1
 import sys
 import math
 sys.path.append('/Users/williamtownsend/Desktop/SchoolWork/Fall2022/MATH471')
@@ -9,5 +10,7 @@ fprime = lambda x: math.exp(x)-2*x
 x0 = 0.1
 err=math.pow(10,-6.0)
 
-ex.newtons_method(f, fprime, x0, err,True)
-ex.another_newtons_method(f, fprime, x0, err,True)
+print('newtons_method():')
+ex.newtons_method(f, fprime, x0, err, True)
+print('another_newtons_method():')
+ex.another_newtons_method(f, fprime, x0, err, True)
