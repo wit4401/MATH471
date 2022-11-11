551c280007cd68b967d0c3a304f99001d3f185a9
wit4401 <wtownsend4401@gmail.com>
2022-11-11 11:51:25 -0500

    started 3.4 problems

---
+++ Chapter3/Chp3.4/example.py
@@ -0,0 +1,6 @@
+import sys
+import math
+sys.path.append('/Users/williamtownsend/Desktop/SchoolWork/Fall2022/MATH471')
+import coursefuncts.chp3 as ex
+
+ex.newtons_division(1/7, math.pow(10,-1.0),True)
---
+++ Chapter3/Chp3.4/problem3_4_05.py
@@ -0,0 +1,12 @@
+import sys
+import math
+sys.path.append('/Users/williamtownsend/Desktop/SchoolWork/Fall2022/MATH471')
+import coursefuncts.chp3 as ex
+
+print('Problem 3.4.5\ta=4/3')
+ex.newtons_division(4/3, math.pow(10,-1.0),True)
+
+print('')
+
+print('Problem 3.4.5\ta=2/3')
+ex.newtons_division(4/3, math.pow(10,-1.0),True)
