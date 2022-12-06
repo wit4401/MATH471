8f772002e8cffd35d12bbcd3eb00b83338180dea
wit4401 <wtownsend4401@gmail.com>
2022-12-06 11:45:43 -0500

    mod to example program for section 3.9

--- Chapter3/Chp3.09/example.py
+++ Chapter3/Chp3.09/example.py
@@ -5,4 +5,4 @@ import matplotlib.pyplot as plt
 sys.path.append('/Users/williamtownsend/Desktop/SchoolWork/Fall2022/MATH471')
 import coursefuncts.chp3 as ex
 
-print("alpha: {}".format(ex.fixed_point_solver(1.01, 1, 2, lambda x:math.exp(-1.0*x)+1,err=math.pow(10,-10.0),verbose=True)))
+print("alpha: {}".format(ex.fixed_point_solver(1.5, 1, 2, lambda x:math.exp(-1.0*x)+1,err=math.pow(10,-10.0),verbose=True)))
